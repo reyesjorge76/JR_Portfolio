e8085b580f613671030f7cd539a1ff0da93a67ef
jorge reyes <reyesjorge76@hotmail.com>
2025-10-30 00:01:58 -0400

    corrected Dynamic Base name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.NODE_ENV === 'production' ? '/my-portfolio/' : '/', // 👈 dynamic base
+  base: process.env.NODE_ENV === 'production' ? '/JR_Portfolio/' : '/', // 👈 dynamic base
   server: {
     host: "::",
     port: 8080,
